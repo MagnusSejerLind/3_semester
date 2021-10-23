d5d80b90ebd35fa5c8a8c179b8846c2b0af88172
MagnusSejerLind <82442041+MagnusSejerLind@users.noreply.github.com>
2021-10-23 15:55:09 +0200

    Add files via upload

---
+++ Assignment_6_7_27.m
@@ -0,0 +1,31 @@
+
+
+
+P=1;
+A=1;
+theta = linspace(0,90,90);
+
+% Avg. normal stress
+sigma = (P*sind(theta).^2)/A;
+
+% Avg. shear stress
+tau = (P/2*A)*sind(2.*theta);
+
+figure(1)
+subplot(2,1,1)      
+plot(sigma,'LineWidth',2)
+xlabel('Θ')
+title('Average normal stress')
+yticks([ ])
+xlim([0 90])
+ylim([0 1.1])
+
+subplot(2,1,2)      
+plot(tau,'LineWidth',2)
+xlabel('Θ')
+title('Average shear stress')
+yticks([ ])
+xlim([0 90])
+ylim([0 0.55])
+
+
