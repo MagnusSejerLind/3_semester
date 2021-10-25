848a8e2530925bea2a9bafc3600c9f24c1f58a8e
MagnusSejerLind <82442041+MagnusSejerLind@users.noreply.github.com>
2021-10-25 13:36:17 +0200

    Add files via upload

---
+++ Fluid_homework_3.m
@@ -0,0 +1,47 @@
+%%
+[x,y] = meshgrid(linspace(0,1.5,200),linspace(0,1.5,200));
+[X,Y] = meshgrid(linspace(0,1.5,15),linspace(0,1.5,15));
+
+A=1; 
+% Constant
+
+y_stag = 1E-3;
+x_stag = 1E-3;
+% Stagnation at (0,0)
+
+u_x = -A.*X;
+u_y = A.*Y;
+% Velocity components
+
+psi = -A.*x.*y;
+% Streamfunction
+
+figure(1)
+[x,y] = contourf(x,y,psi,'LevelStep',0.1,'LineWidth',1);
+
+hold on
+
+
+
+plot(x_stag,y_stag,'ro','LineWidth',2)
+
+plot(1,1,'ro','LineWidth',2)
+
+clabel(x,y)
+
+
+xlim([0.001 1.3])
+ylim([0.001 1.5])
+
+[x,y] = meshgrid(linspace(0,1.5,200),linspace(0,1.5,200));
+
+contour(x,y,psi,'r','LevelStep',1,'LineWidth',2);
+
+
+
+
+quiver(X,Y,u_x,u_y,'k');
+
+
+
+
