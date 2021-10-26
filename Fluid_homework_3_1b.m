e0172a624ec66ff339e918f22c4de9c9811c46ae
MagnusSejerLind <82442041+MagnusSejerLind@users.noreply.github.com>
2021-10-26 22:19:49 +0200

    Update Fluid_homework_3_1b.m

--- Fluid_homework_3_1b.m
+++ Fluid_homework_3_1b.m
@@ -1,4 +1,4 @@
-%%
+
 [x,y] = meshgrid(linspace(0,1.5,200),linspace(0,1.5,200));
 [X,Y] = meshgrid(linspace(0,1.5,15),linspace(0,1.5,15));
 
@@ -17,7 +17,7 @@ psi = -A.*x.*y;
 % Streamfunction
 
 figure(1)
-[x,y] = contourf(x,y,psi,'LevelStep',0.1,'LineWidth',1);
+[x,y] = contour(x,y,psi,'LevelStep',0.1,'LineWidth',1);
 clabel(x,y)
 
 hold on
@@ -25,12 +25,20 @@ plot(x_stag,y_stag,'ro','LineWidth',2)
 
 plot(1,1,'ro','LineWidth',2)
 
-xlim([0.001 1.3])
-ylim([0.001 1.5])
+xlim([1E-3 1.3])
+ylim([1E-3 1.5])
 
 [x,y] = meshgrid(linspace(0,1.5,200),linspace(0,1.5,200));
 contour(x,y,psi,'r','LevelStep',1,'LineWidth',2);
-% Streamline at psi=1
+% Streamline at psi = 1
 
-quiver(X,Y,u_x,u_y,'k');
+quiver(X,Y,u_x,u_y,'k','LineWidth',1);
 % Vector field of flow
+
+title('Flow Field')
+
+xlabel('x')
+ylabel('y')
+
+hYLabel = get(gca,'YLabel');
+ set(hYLabel,'rotation',0,'VerticalAlignment','middle')
