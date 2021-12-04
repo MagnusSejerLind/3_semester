8dc22f756f866df936fc29aaf13f74c7b30f86ff
MagnusSejerLind <82442041+MagnusSejerLind@users.noreply.github.com>
2021-12-04 12:10:03 +0100

    Update Assignment_11_12_18.m

--- Assignment_11_12_18.m
+++ Assignment_11_12_18.m
@@ -6,13 +6,11 @@ c = 100;
 
 tau = (4*V)./(3*pi.*c.^4).*(c.^2-y.^2);
 
-
 plot(tau,y,LineWidth=2)
 
 grid on
 title('Shear stress distrubition')
 xlabel('Shear stress [MPa]')
-ylabel('y [m]')
+ylabel('y [mm]')
 hYLabel = get(gca,'YLabel');
 set(hYLabel,'rotation',0,'VerticalAlignment','middle')
-
